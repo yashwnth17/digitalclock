17dd1ed87841db450b768b4bc38a24648944304c
yashwanth <kavitha.manju.l1972@gmail.com>
2023-12-23 21:42:28 +0530

    taskof the day

---
+++ clock.js
@@ -0,0 +1,25 @@
+function currentTime() {
+    let date = new Date(); 
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; 
+      let t = setTimeout(function(){ currentTime() }, 2000); 
+  }
+  
+  function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
+    }
+    else {
+      return k;
+    }
+  }
+  
+  currentTime(); 
